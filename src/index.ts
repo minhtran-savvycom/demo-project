5aae38e59fe5a2cfe78e95bb17b5f35b82434696
minh <minhtranconglis@gmail.com>
2024-06-13 18:09:19 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ dataSource
 const specs = swaggerJsdoc(options);
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT || 3000;
 
 app.use(express.json());
 
--- src/setting/configuration.ts
+++ src/setting/configuration.ts
@@ -1,12 +1,15 @@
 import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const dataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '123456',
-  database: 'demo-db',
+  type: process.env.DB_TYPE as any,
+  host: process.env.DB_HOST as any,
+  port: process.env.DB_PORT as any,
+  username: process.env.DB_USER,
+  password: process.env.DB_ROOT_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ['src/shared/entities/*.entity.ts'],
   logging: false,
   synchronize: true,
