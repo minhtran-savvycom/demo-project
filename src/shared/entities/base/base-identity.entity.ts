71b512f2646ca2b2ea0b8ea47360f22fcdb0442a
minh <minhtranconglis@gmail.com>
2024-06-17 10:00:42 +0700

    update

--- src/shared/entities/base/base-identity.entity.ts
+++ src/shared/entities/base/base-identity.entity.ts
@@ -1,6 +1,30 @@
-import { PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 export class BaseIdentityEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @CreateDateColumn({
+    name: 'createdAt',
+    type: 'date',
+  })
+  @Exclude()
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updatedAt', type: 'date' })
+  @Exclude()
+  updatedAt: Date;
+
+  @Exclude()
+  @DeleteDateColumn({
+    name: 'deletedAt',
+    type: 'date',
+  })
+  deletedAt?: Date;
 }
